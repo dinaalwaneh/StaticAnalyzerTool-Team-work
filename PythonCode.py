b9c4d5f38fa696fb76ed757be67dd961999eb75f
eleenkmail <79339710+eleenkmail@users.noreply.github.com>
2022-03-08 00:26:30 +0200

    Python Code for Test

---
+++ PythonCode.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+
+class program:
+ 
+    
+    def Calculate(self, number,  divisor):
+        result = number/divisor;
+        return result
+
+
+    def statusValue(status):
+        if(status == -1 ):
+            print("faild")
+        elif(status == 1):
+            print("success")      
+        elif (status == 0):
+            print("invalid")
+            
+            
+            
+    def checkStatus():
+        status_value(2,5)
+        
+    
+    def parameters(param1, param2, param3, param4):
+        return 1
+    
+    
+    def unReachable(num):
+        if(num>40):
+            if(num >30):
+                return 1
+            
+            
+    def  Print(self,student):
+        print("Student", student.name)
+        
+
+
+
+
+
+
+
