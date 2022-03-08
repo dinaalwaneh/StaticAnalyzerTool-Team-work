26faf8067e6affe303dd19878d2c3bf8011d54d7
eleenkmail <79339710+eleenkmail@users.noreply.github.com>
2022-03-08 22:02:36 +0200

    update python code .py

--- PythonCode.py
+++ PythonCode.py
@@ -4,36 +4,36 @@
 class program:
  
     
-    def Calculate(self, number,  divisor):
+    def Calculate(number:int,divisor:int):
         result = number/divisor;
         return result
 
 
-    def GetStatusValue(status):
-        if(status == -1 ):
+    def GetStatusValue(status:int):
+        if status == -1 :
             print("faild")
-        elif(status == 1):
+        elif status == 1:
             print("success")      
-        elif (status == 0):
+        elif status == 0:
             print("invalid")
             
             
             
     def checkStatus():
-        status_value(2,5)
+        GetStatusValue(2,5)
         
     
-    def parameters(param1, param2, param3, param4):
+    def parameters(param1:int,param2:str,param3:float,param4:int):
         return 1
     
     
-    def unReachable(num):
-        if(num>40):
-            if(num >30):
+    def unReachable(num:int):
+        if num > 40:
+            if num > 30:
                 return 1
             
             
-    def  Print(self,student):
+    def Print(student:object):
         print("Student", student.name)
         
     
